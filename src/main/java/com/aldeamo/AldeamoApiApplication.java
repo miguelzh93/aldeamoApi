34ed89620886e892dc86c8a163c1b0cb10bd0ec8
miguelzh93 <miguel_zh93@hotmail.com>
2022-09-05 19:13:46 -0500

    Estructura inicial

---
+++ src/main/java/com/aldeamo/AldeamoApiApplication.java
@@ -0,0 +1,13 @@
+package com.aldeamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldeamoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldeamoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldeamo/AldeamoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldeamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldeamoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
